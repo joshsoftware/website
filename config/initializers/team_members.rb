1aa46dbe10dca4f9079a228cffa5d024e1c0e3f6
Sethupathi Asokan <sethu@joshsoftware.com>
2012-09-18 20:18:10 +0530

    Changed the team content and contact detail
    - Change the content for team members and view page
    - Added contact detail in the home page
    - Added follow twitter, facebook like and blog subscripe in the footer,
        but the alignment needs to be fixed

--- config/initializers/team_members.rb
+++ config/initializers/team_members.rb
@@ -4,7 +4,7 @@ TEAM_MEMBERS= [
     name: "Gautam Rege",
     title: "Co-Founder & Director",
     image: "images/gautam.jpg",
-    description: "In his 13+ years in the Software Industry, he has worked with product Companies like Veritas Software Inc. (now Symantec Inc) and services companies like Zensar Technologies and Cybage Software. He is an author of <a href='http://www.packtpub.com/ruby-and-mongodb-web-development-beginners-guide/book'> 'Ruby and MongoDB Web Development'</a>. He has graduated in 2000 with a Bachelors Degree in Computer Engineering from Pune Institute of Computer Technology (PICT), India. You can find his blog here and can follow him on twitter at @gautamrege",
+    description: "In his 13+ years in the Software Industry, he has worked with product Companies like Veritas Software Inc. (now Symantec Inc) and services companies like Zensar Technologies and Cybage Software. He is the author of <a target='_blank' href='http://www.packtpub.com/ruby-and-mongodb-web-development-beginners-guide/book'> 'Ruby and MongoDB Web Development'</a>. He has graduated in 2000 with a Bachelors Degree in Computer Engineering from Pune Institute of Computer Technology (PICT), India.",
     technical: {
         twitter: "@gautamrege",
         linkedin: "http://www.linkedin.com/in/gautamrege",
@@ -19,7 +19,7 @@ TEAM_MEMBERS= [
     name: "Sethupathi Asokan",
     title: "Co-Founder & Director",
     image: "images/sethu.jpg",
-    description: "In his 15+ years, he has worked all along with Veritas Software (now Symantec Inc). He was instrumental in designing and managing Veritas Command Central Storage and was the senior most member of the team of 160 engineers. He holds a Masters Degree in Computer Science. You can follow him on twitter @sethupathia",
+    description: "In his 15+ years, he has worked all along with Veritas Software (now Symantec Inc). He was instrumental in designing and managing Veritas Command Central Storage and was the senior most member of the team of 160 engineers. He has done Masters Degree in Computer Science in 1998.",
     technical: {
         twitter: "@sethupathia",
         linkedin: "http://www.linkedin.com/in/sethupathia",
@@ -34,7 +34,7 @@ TEAM_MEMBERS= [
     name: "Satish Talim",
     title: "Director",
     image: "images/satish.jpg",
-    description: "Satish is a Ruby Evangelist, programmer, author, trainer and speaker. A recognized expert in the field of software development with over 32+ years of I.T. experience, Satish has consulted and trained teams at various companies in India and the US. He is the owner of Rubylearning.com and Rubylearning.org and is one of the most popular personalities in the Ruby world. You can follow him on twitter @IndianGuru",
+    description: "Satish is a Ruby Evangelist, programmer, author, trainer and speaker. A recognized expert in the field of software development with over 32+ years of I.T. experience, Satish has consulted and trained teams at various companies in India and the US. He is the owner of Rubylearning.com and Rubylearning.org and is one of the most popular personalities in the Ruby world.",
     technical: {
         twitter: "@IndianGuru",
         linkedin: "http://www.linkedin.com/in/satishtalim",
