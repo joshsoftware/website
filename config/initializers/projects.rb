fd7a1a4fdbf39fb053232f538ab4ec31daf35b9c
Gautam Rege <gautam@joshsoftware.com>
2012-09-23 19:45:00 +0530

    Adding project details (WIP)

--- config/initializers/projects.rb
+++ config/initializers/projects.rb
@@ -1,14 +1,14 @@
 PROJECTS= [
   { 
     id: "the-deal-pages",
-    title: "TheDealPages",
+    title: "The Deal Pages",
     url: "http://www.thedealpages.com",
-    image: "images/porfolio/joshsoftware_deal-pages.jpg",
-    description: "Is your one-stop directory for all the best daily deals in your city. Browse hundreds of limited time offers to purchase great deals from local business for 40-90% off!",
+    image: "assets/portfolio/joshsoftware_deal-pages.png",
+    description: "TheDealPages a one-stop directory for all the best daily deals in your city. You can browse hundreds of limited time offers to purchase great deals from local business for 40-90% off!",
     technical: {
-        stack: "Rails 3.0.8, thini, aaahhh",
-        deployment: "Heroku",
-        comments: ""
+        stack: "Rails 3.0.8, PostgreSQL",
+        deployment: "Heroku Bamboo stack",
+        comments: "It uses <a href='http://github.com/jiren/filter.js'>filter.js </a>"
     },
     display: true
   },
@@ -17,13 +17,117 @@ PROJECTS= [
     id: "cleartrip",
     title: "ClearTrip",
     url: "http://hotel.cleartrip.com",
-    image: "images/porfolio/joshsoftware_clear-trip.jpg",
-    description: "This is a hotel directory listing for Cleartrip. Very heavy caching enables hotels search very fast and reliable. This page is used in A/B testing for Cleartrip. Localities are mapped and nearby hotels are cached via XML api access using Cleartrip APIs. Hosting this directly on an EC2 instance provides flexibility to cache it entirely.",
+    image: "assets/portfolio/joshsoftware_clear-trip.png",
+    description: "This is a hotel directory listing for Cleartrip. Very heavy caching enables hotels search very fast and reliable. This page is used in A/B testing for Cleartrip. Localities are mapped and nearby hotels are cached via XML api access using Cleartrip APIs.",
     technical: {
-        stack: "Rails 3.0.7, Memcached, nginx + passgenger",
-        deployments: "Amazon EC2",
+        stack: "Rails 3.0.7, MySQL, Memcached, nginx + passgenger",
+        deployment: "Amazon EC2",
         comments: ""
     },
     display: true
+  },
+
+  {
+    id: "tourconnect",
+    title: "TourConnect",
+    url: "http://tourconnect.com",
+    image: "assets/portfolio/joshsoftware_clear-trip.png",
+    description: "TourConnect makes managing rates and contracting quick and easy! TourConnect communicates what the Supplier product is and how much it's going to cost each Contractor",
+    technical: {
+        stack: "Rails 3.1, MongoDB",
+        deployment: "Heroku Cedar Stack",
+        comments: ""
+    },
+    display: true
+  },
+
+  {
+    id: "1factory",
+    title: "1Factory",
+    url: "http://1factory.com",
+    image: "assets/portfolio/joshsoftware_clear-trip.png",
+    description: "1Factory is a portal that manages the quality control process for multi-tiered supply chain. This is a multi-tenant application with a lot of impetus on security.",
+    technical: {
+        stack: "Rails 3.1, PostgreSQL",
+        deployment: "Heroku Cedar Stack",
+        comments: ""
+    },
+    display: true
+  },
+
+  {
+    id: "groupgyaan",
+    title: "GroupGyaan",
+    url: "http://groupgyaan.com",
+    image: "assets/portfolio/joshsoftware_clear-trip.png",
+    description: "GroupGyaan is a next generation marketplace for classroom sessions. You can book tickets for sessions on things that may be of interest to you, from a variety of different areas such as photography, cooking, parenting or negotiation skills.",
+    technical: {
+        stack: "Rails 3.1, PostgreSQL",
+        deployment: "Heroku Cedar Stack",
+        comments: ""
+    },
+    display: true
+  },
+
+  {
+    id: "moveme",
+    title: "MoveMe Planner",
+    url: "http://planner.moveme.com",
+    image: "assets/portfolio/joshsoftware_clear-trip.png",
+    description: "This site helps in moving house in UK. You can get instant quotes from vendors for insurance, man and van etc. The Free House Moving Checklist is an ideal planner for managing your home move.<br> This is a while-lable solution for various portals.",
+    technical: {
+        stack: "Rails ???, MySQL",
+        deployment: "Private Hosting",
+        comments: ""
+    },
+    display: true
+  },
+
+  {
+    id: "iPALM",
+    title: "iPALM",
+    url: "http://www.opusgroup.com.au/ipalm",
+    image: "assets/portfolio/joshsoftware_clear-trip.png",
+    description: "'Integrated Print & Logistics Management' (iPALM) is a proprietary Technology platform. It moves traditional print, warehousing, and fulfilment capabilities online, providing customers with access to storefronts, backlist catalogues, fulfillment, tracking, reporting and print on demand.",
+    technical: {
+        stack: "Rails ???, MySQL, Redis",
+        deployment: "Private Hosting",
+        comments: ""
+    },
+    display: true
+  },
+
+  {
+    id: "brandpotion",
+    title: "BrandPotion",
+    url: "http://www.brandpotion.com",
+    image: "assets/portfolio/joshsoftware_clear-trip.png",
+    description: "BrandPotion brings brands to the creative community via competitions. It takes networking beyond the social realm - we call this Brand networking! Features like ratings, surveys, photo-gallery, video-gallery and forums can be enabled or disabled per brand. So each brand has its own unique identity",
+    technical: {
+        stack: "Rails 2.2.2, MySQL",
+        deployment: "Engineyard xCloud",
+        comments: ""
+    },
+    display: true
+  },
+
+  {
+    id: "samhita",
+  },
+
+  {
+    id: "globalindiafund",
+  },
+
+  {
+    id: "6ycollective",
+  },
+
+  {
+    id: "perfect-pet-match",
+  },
+
+  {
+    id: "qwikcompliance",
   },
 ]
