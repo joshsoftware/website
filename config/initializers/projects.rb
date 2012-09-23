cb94d253fff190e8b8cb0d227f386c6279686c73
Gautam Rege <gautam@joshsoftware.com>
2012-09-23 22:38:43 +0530

    Adding country and year to the projects

--- config/initializers/projects.rb
+++ config/initializers/projects.rb
@@ -2,6 +2,8 @@ PROJECTS= [
   { 
     id: "the-deal-pages",
     title: "The Deal Pages",
+    country: "Canada",
+    year: "2011",
     url: "http://www.thedealpages.com",
     image: "assets/portfolio/joshsoftware_deal-pages.png",
     description: "TheDealPages a one-stop directory for all the best daily deals in your city. You can browse hundreds of limited time offers to purchase great deals from local business for 40-90% off!",
@@ -16,6 +18,8 @@ PROJECTS= [
   { 
     id: "cleartrip",
     title: "ClearTrip",
+    country: "India",
+    year: "2011",
     url: "http://hotel.cleartrip.com",
     image: "assets/portfolio/joshsoftware_clear-trip.png",
     description: "This is a hotel directory listing for Cleartrip. Very heavy caching enables hotels search very fast and reliable. This page is used in A/B testing for Cleartrip. Localities are mapped and nearby hotels are cached via XML api access using Cleartrip APIs.",
@@ -30,6 +34,8 @@ PROJECTS= [
   {
     id: "tourconnect",
     title: "TourConnect",
+    country: "Australia",
+    year: "2012",
     url: "http://tourconnect.com",
     image: "assets/portfolio/joshsoftware_clear-trip.png",
     description: "TourConnect makes managing rates and contracting quick and easy! TourConnect communicates what the Supplier product is and how much it's going to cost each Contractor",
@@ -44,6 +50,8 @@ PROJECTS= [
   {
     id: "1factory",
     title: "1Factory",
+    country: "USA",
+    year: "2012",
     url: "http://1factory.com",
     image: "assets/portfolio/joshsoftware_clear-trip.png",
     description: "1Factory is a portal that manages the quality control process for multi-tiered supply chain. This is a multi-tenant application with a lot of impetus on security.",
@@ -58,6 +66,8 @@ PROJECTS= [
   {
     id: "groupgyaan",
     title: "GroupGyaan",
+    country: "India",
+    year: "2011",
     url: "http://groupgyaan.com",
     image: "assets/portfolio/joshsoftware_clear-trip.png",
     description: "GroupGyaan is a next generation marketplace for classroom sessions. You can book tickets for sessions on things that may be of interest to you, from a variety of different areas such as photography, cooking, parenting or negotiation skills.",
@@ -72,6 +82,8 @@ PROJECTS= [
   {
     id: "moveme",
     title: "MoveMe Planner",
+    country: "UK",
+    year: "2011",
     url: "http://planner.moveme.com",
     image: "assets/portfolio/joshsoftware_clear-trip.png",
     description: "This site helps in moving house in UK. You can get instant quotes from vendors for insurance, man and van etc. The Free House Moving Checklist is an ideal planner for managing your home move.<br> This is a while-lable solution for various portals.",
@@ -86,6 +98,8 @@ PROJECTS= [
   {
     id: "iPALM",
     title: "iPALM",
+    country: "Australia",
+    year: "2012",
     url: "http://www.opusgroup.com.au/ipalm",
     image: "assets/portfolio/joshsoftware_clear-trip.png",
     description: "'Integrated Print & Logistics Management' (iPALM) is a proprietary Technology platform. It moves traditional print, warehousing, and fulfilment capabilities online, providing customers with access to storefronts, backlist catalogues, fulfillment, tracking, reporting and print on demand.",
@@ -100,6 +114,8 @@ PROJECTS= [
   {
     id: "brandpotion",
     title: "BrandPotion",
+    country: "Italy, India",
+    year: "2010",
     url: "http://www.brandpotion.com",
     image: "assets/portfolio/joshsoftware_clear-trip.png",
     description: "BrandPotion brings brands to the creative community via competitions. It takes networking beyond the social realm - we call this Brand networking! Features like ratings, surveys, photo-gallery, video-gallery and forums can be enabled or disabled per brand. So each brand has its own unique identity",
