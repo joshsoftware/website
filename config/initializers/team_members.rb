873be1b569236f9654c5aae0bf3d75af0952374d
Sethupathi Asokan <sethu@joshsoftware.com>
2012-09-26 17:15:33 +0530

    Rearranged images folder
    -Add flag icons for country

--- config/initializers/team_members.rb
+++ config/initializers/team_members.rb
@@ -3,7 +3,7 @@ TEAM_MEMBERS= [
     id: "gautam-rege",
     name: "Gautam Rege",
     title: "Co-Founder & Director",
-    image: "images/gautam.jpg",
+    image: "assets/team/gautam.jpg",
     description: "In his 13+ years in the Software Industry, he has worked with product Companies like Veritas Software Inc. (now Symantec Inc) and services companies like Zensar Technologies and Cybage Software. He is the author of <a target='_blank' href='http://www.packtpub.com/ruby-and-mongodb-web-development-beginners-guide/book'> 'Ruby and MongoDB Web Development'</a>. He has graduated in 2000 with a Bachelors Degree in Computer Engineering from Pune Institute of Computer Technology (PICT), India.",
     technical: {
         twitter: "@gautamrege",
@@ -18,7 +18,7 @@ TEAM_MEMBERS= [
     id: "sethupathi-asokan",
     name: "Sethupathi Asokan",
     title: "Co-Founder & Director",
-    image: "images/sethu.jpg",
+    image: "assets/team/sethu.jpg",
     description: "In his 15+ years, he has worked all along with Veritas Software (now Symantec Inc). He was instrumental in designing and managing Veritas Command Central Storage and was the senior most member of the team of 160 engineers. He has done Masters Degree in Computer Science in 1998.",
     technical: {
         twitter: "@sethupathia",
@@ -33,7 +33,7 @@ TEAM_MEMBERS= [
     id: "satish-talim",
     name: "Satish Talim",
     title: "Director",
-    image: "images/satish.jpg",
+    image: "assets/team/satish.jpg",
     description: "Satish is a Ruby Evangelist, programmer, author, trainer and speaker. A recognized expert in the field of software development with over 32+ years of I.T. experience, Satish has consulted and trained teams at various companies in India and the US. He is the owner of Rubylearning.com and Rubylearning.org and is one of the most popular personalities in the Ruby world.",
     technical: {
         twitter: "@IndianGuru",
