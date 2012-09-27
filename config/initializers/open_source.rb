4323d232eb355e4d01b2a39c3508f2cea83541f2
Gautam Rege <gautam@joshsoftware.com>
2012-09-27 11:04:43 +0530

    OpenSource details

---
+++ config/initializers/open_source.rb
@@ -0,0 +1,63 @@
+OPEN_SOURCE= [
+  {
+    id: "open-source-fridays",
+    title: "Hosting Open Source Fridays",
+    url: "http://www.meetup.com/PuneRailsMeetup/",
+    details: "A few months ago we started a movement - <b>doing things differently</b>. Every Friday evening, we do different things - working on an open-source gem, doing open-source projects, learning new things and having fun. <br><br> Its actually making a difference - its now open to the community.",
+    display: true
+  },
+
+  {
+    id: "filterjs",
+    title: "Filter.js",
+    url: "https://github.com/jiren/filter.js",
+    details: "Filter.js is the javascript library for client-side searches. It has multiple filter criteria that can be specified and used in conjunction with each other. It has easy integration with jQuery templates and mustache. See the demo <a href='http://jiren.github.com/filter.js/filterjs.html'>here</a> <br> Its been covered by <a href='http://webscripts.softpedia.com/script/Forms-and-Controls-C-C/Filter-js-71265.html'>Softpedia</a> and <a href='http://twitter.com/smashingmag/status/122082466726019072'>Smashing Magazine</a>", 
+    display: true
+  },
+
+  {
+    id: "feather_cms",
+    title: "Feather CMS",
+    url: "https://github.com/joshsoftware/feather_cms",
+    details: "We have felt the pain of integrating static pages into a Rails application. There are plenty of gems available for this (Radiant, Locomotiv, etc.) but they are very heavy for what we need.<br> So, we built FeatherCMS. As the name suggests, its a Do-It-Yourself, lightweight CMS - just pages, caching and nothing more!",
+    display: true
+  },
+
+  {
+    id: "splat",
+    title: "SPlat - SMS Platform",
+    url: 'http://rubygems.org/gems/josh-splat',
+    details: "SPlat is an integration platform to make use of SMS integration really easy. It ensures single point of integration, supports changing vendors without changing code, sending and receiving SMS, scheduled delivery and tagged groups. It isn't a SMS gateway but a generic layer on top of it. It was presented by Gautam at Lone Star Ruby Conference, 2010.",
+    display: true
+  },
+
+  {
+    id: "sso-devise-omniauth",
+    title: "Single Sign On using Devise and Omniauth",
+    url: "https://github.com/joshsoftware/sso-devise-omniauth-provider",
+    details: "We wanted to solve these problems:<br><ul>
+      <li>A single User Manager application (which will authenticate ALL users with different roles)</li>
+      <li>Different internal applications which talk to User Manager for authentication</li>
+      <li>User should be able to login/sign-up via Social Networks like Twitter and Facebook.</li>
+      <li>Single Sign On between all applications.</li>",
+    display: true
+  },
+
+  {
+    id: "eJosh",
+    title: "eJosh - custom CMS/ERP for SME",
+    url: "https://github.com/joshsoftware/eJosh",
+    details: "eJOSH is a custom CMS. eJOSH is Rails application that comes with a list of pre-defined layouts and color themes and it is ready to use to build your website or to use for your intranet with it's available plugins (employee management, Leave management, Timesheet management, etc..) and it has built in user management (with 3 different roles).<br> It has built in dynamic sitemap generation for SEO compliance. Capistrano default configuration is included that can be modified to fit your needs.",
+    display: true
+  },
+
+  {
+    id: "website",
+    title: "This very website!",
+    url: "https://github.com/joshsoftware/website",
+    details: "We are all about Ruby! Even <a href='http://www.joshsoftware.com'>this website</a> itself is open-source! It's an example of our work - Twitter Bootstrap, coffeescript, scss and Rails asset pipeline in action! ",
+    display: true
+  },
+
+
+]
