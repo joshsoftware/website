1f0e6af37d88a30fdb91855a37c97e24c2af514a
Gautam Rege <gautam@joshsoftware.com>
2012-09-27 13:28:05 +0530

    Updating Open Source projects with github info

--- config/initializers/open_source.rb
+++ config/initializers/open_source.rb
@@ -1,16 +1,20 @@
 OPEN_SOURCE= [
   {
     id: "open-source-fridays",
-    title: "Hosting Open Source Fridays",
+    title: "<img src='http://img2.meetupstatic.com/906521611995523788/img/header/logo.png' width='40px' height='30px'/>  Hosting Open Source Fridays ",
     url: "http://www.meetup.com/PuneRailsMeetup/",
-    details: "A few months ago we started a movement - <b>doing things differently</b>. Every Friday evening, we do different things - working on an open-source gem, doing open-source projects, learning new things and having fun. <br><br> Its actually making a difference - its now open to the community.",
+    details: "A few months ago we started a movement - <b>doing things differently</b>. Every Friday evening, we do different things - working on an open-source gem, doing open-source projects, learning new things and having fun. <br> Its actually making a difference - its now open to the community. ",
     display: true
   },
 
   {
     id: "filterjs",
     title: "Filter.js",
-    url: "https://github.com/jiren/filter.js",
+    url: "https://jiren.github.com/filter.js",
+    github: {
+      user: 'jiren',
+      repo: 'filter.js'
+    },
     details: "Filter.js is the javascript library for client-side searches. It has multiple filter criteria that can be specified and used in conjunction with each other. It has easy integration with jQuery templates and mustache. See the demo <a href='http://jiren.github.com/filter.js/filterjs.html'>here</a> <br> Its been covered by <a href='http://webscripts.softpedia.com/script/Forms-and-Controls-C-C/Filter-js-71265.html'>Softpedia</a> and <a href='http://twitter.com/smashingmag/status/122082466726019072'>Smashing Magazine</a>", 
     display: true
   },
@@ -19,7 +23,59 @@ OPEN_SOURCE= [
     id: "feather_cms",
     title: "Feather CMS",
     url: "https://github.com/joshsoftware/feather_cms",
+    github: {
+      user: 'joshsoftware',
+      repo: 'feather_cms'
+    },
     details: "We have felt the pain of integrating static pages into a Rails application. There are plenty of gems available for this (Radiant, Locomotiv, etc.) but they are very heavy for what we need.<br> So, we built FeatherCMS. As the name suggests, its a Do-It-Yourself, lightweight CMS - just pages, caching and nothing more!",
+    display: true
+  },
+
+  {
+    id: "punerb",
+    title: "Pune Ruby Group",
+    url: "http://puneruby.org",
+    github: {
+      user: 'punerb',
+      repo: 'ideahub'
+    },
+    details: "We have help build the PuneRB site. This is a portal where we share ideas and work on them. This keeps the community going. It has a unique 'only-twitter' authentication and does not save any email information.",
+    display: true
+  },
+
+  {
+    id: "supportbee",
+    title: "SupportBee",
+    url: "http://supportbee.com",
+    github: {
+      user: 'punerb',
+      repo: 'supportbee'
+    },
+    details: "We are helping to build the supportbee client gem. This is a command line tool that can fetch tickets, see owners, assign labels, generate reports all from command line.",
+    display: true
+  },
+
+  {
+    id: "blitzio",
+    title: "Blitz.io",
+    url: "http://blitz.io",
+    github: {
+      user: 'punerb',
+      repo: 'blitz-ruby'
+    },
+    details: "We have been helping blitz.io update their gem with peripheral functionality. This is as part of Open Source Friday meetups.",
+    display: true
+  },
+
+  {
+    id: "ispunity",
+    title: "ISPunity",
+    url: "http://rubygems.org/gems/ispunity",
+    github: {
+      user: 'aruntomar',
+      repo: 'isp_unity'
+    },
+    details: "ISPunity is an open source application that integrates multiple Internet connections for load balancing and failover. It 'magically' balances the Internet load over these different connections. This also means that the more connections we use, the faster and better connectivity we get! It also automatically handles failover to another connection in case any of the lines go down ",
     display: true
   },
 
@@ -27,6 +83,10 @@ OPEN_SOURCE= [
     id: "splat",
     title: "SPlat - SMS Platform",
     url: 'http://rubygems.org/gems/josh-splat',
+    github: {
+      user: 'joshsoftware',
+      repo: 'feather_cms'
+    },
     details: "SPlat is an integration platform to make use of SMS integration really easy. It ensures single point of integration, supports changing vendors without changing code, sending and receiving SMS, scheduled delivery and tagged groups. It isn't a SMS gateway but a generic layer on top of it. It was presented by Gautam at Lone Star Ruby Conference, 2010.",
     display: true
   },
@@ -34,7 +94,11 @@ OPEN_SOURCE= [
   {
     id: "sso-devise-omniauth",
     title: "Single Sign On using Devise and Omniauth",
-    url: "https://github.com/joshsoftware/sso-devise-omniauth-provider",
+    url: "http://blog.joshsoftware.com/2010/12/16/multiple-applications-with-devise-omniauth-and-single-sign-on/",
+    github: {
+      user: 'joshsoftware',
+      repo: 'sso-devise-omniauth-provider'
+    },
     details: "We wanted to solve these problems:<br><ul>
       <li>A single User Manager application (which will authenticate ALL users with different roles)</li>
       <li>Different internal applications which talk to User Manager for authentication</li>
@@ -47,6 +111,10 @@ OPEN_SOURCE= [
     id: "eJosh",
     title: "eJosh - custom CMS/ERP for SME",
     url: "https://github.com/joshsoftware/eJosh",
+    github: {
+      user: 'joshsoftware',
+      repo: 'eJosh'
+    },
     details: "eJOSH is a custom CMS. eJOSH is Rails application that comes with a list of pre-defined layouts and color themes and it is ready to use to build your website or to use for your intranet with it's available plugins (employee management, Leave management, Timesheet management, etc..) and it has built in user management (with 3 different roles).<br> It has built in dynamic sitemap generation for SEO compliance. Capistrano default configuration is included that can be modified to fit your needs.",
     display: true
   },
@@ -54,10 +122,12 @@ OPEN_SOURCE= [
   {
     id: "website",
     title: "This very website!",
-    url: "https://github.com/joshsoftware/website",
+    url: "https://www.joshsoftware.com",
+    github: {
+      user: 'joshsoftware',
+      repo: 'website'
+    },
     details: "We are all about Ruby! Even <a href='http://www.joshsoftware.com'>this website</a> itself is open-source! It's an example of our work - Twitter Bootstrap, coffeescript, scss and Rails asset pipeline in action! ",
     display: true
   },
-
-
 ]
