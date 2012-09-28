f4f7da6e3a95340c1e25fb704a2bceb2e7043a4e
Jiren Patel <jiren@joshsoftware.com>
2012-09-28 16:04:09 +0530

    Jiren Profile Update

--- config/initializers/team_members.rb
+++ config/initializers/team_members.rb
@@ -276,7 +276,7 @@ TEAM_MEMBERS= [
     name: "Jiren Patel",
     title: "Senior Software Engineer",
     image: "http://www.gravatar.com/avatar/dde56ce7a6860df43fc2eee1336552dc",
-    description: "Jiren is a passionate and enthusiastic about programming languages. He is avid coder, biker, gamer and loves to discover new things. He is the author of many opensouce projects i.e Filter.js, Splat, FeatherCMS. He has Bachelors Degree in Computer Engineering from Dharmsinh Desai University.",
+    description: "Jiren is a passionate and enthusiastic about programming languages. He is avid coder, <a href='http://www.flickr.com/photos/50103686@N03/8032359262/in/set-72157631641674776/' target='_blank'>biker</a>, gamer and loves to discover new things. He is the author of many opensouce projects i.e Filter.js, Splat, FeatherCMS. He has Bachelors Degree in Computer Engineering from Dharmsinh Desai University.",
     technical: {
       github: 'jiren',
       blog: 'http://blog.joshsoftware.com/author/jiren',
