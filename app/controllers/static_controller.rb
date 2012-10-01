2c75536eb6f6f758d640aee62ef5ebd6f9ca971f
Sethupathi Asokan <sethu@joshsoftware.com>
2012-10-01 16:10:28 +0530

    Added meta keywords and meta description

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,10 +3,15 @@ class StaticController < ApplicationController
 
   def portfolio
     @projects = PROJECTS.select { |p| p[:display] }
+    @keywords = "Josh Software portfolio, works we have accompolished, #{@projects.collect{|p| p[:title]}.join(", ")}"
+    @description = "We work in various industry verticals like Insurance, Manufacturing, Media, eCommerce, Social Sector and Information Technology as long as the work is in Ruby."
   end
 
   def team
-    @team_members = TEAM_MEMBERS.sort{|x,y| x[:index] <=> y[:index]}.select { |t| t[:display] } 
+    @team_members = TEAM_MEMBERS.sort{|x,y| x[:index] <=> y[:index]}.select { |t| t[:display] }
+    team =  @team_members.collect{|t| t[:name]}.join(", ")
+    @keywords = "The JOSH People, the team, #{team}"
+    @description = "The JOSH People, the team behind our success. #{team}"
   end
 
   def home
@@ -15,10 +20,14 @@ class StaticController < ApplicationController
 
   def testimonials
     @testimonies = TESTIMONIES.select{|e| e[:display]}
+    @keywords = "Client testimonials"
+    @description = "Nothing certifies our skill other than testimonials, here are the few testimonials from our clients"
   end
 
   def open_source
     @open_sources = OPEN_SOURCE.select{|e| e[:display]}
+    @keywords = "Open source Friday, open source contribution, open source involvement"
+    @description = "Every Friday evening, we do different things - working on an open-source gem, doing open-source projects, learning new things and having fun."
   end
 
   def ping
