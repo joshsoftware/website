378958eecc77e31685424dc86d4f5fe7d671a9fb
Prasad Surase <prasad@joshsoftware.com>
2014-01-03 10:56:49 +0530

    Modified the 'contact us' page

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,9 +1,5 @@
 module StaticHelper
   def check_if_active(tab)
-    Rails.logger.info '================'
-    Rails.logger.info tab
-    Rails.logger.info params[:action]
-
     params[:action] == tab ? 'active' : ''
   end
 end
