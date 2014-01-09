6cbdc4cf2d1ec907c026e3b4cb420ccc7e886df9
Prasad Surase <prasad@joshsoftware.com>
2014-01-09 15:47:44 +0530

    Modified the code to display the tweet if available

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,12 +1,16 @@
 class Tweet
   def self.latest_tweet(name = 'joshsoftware')
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key    = ENV['TWITTER_CONSUMER_KEY']
-      config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+    begin
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key    = ENV['TWITTER_CONSUMER_KEY']
+        config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+      end
+      #ids = client.user_timeline('joshsoftware').collect(&:id)
+      #ids[0..5].collect{|id| client.status(id).full_text}
+      id = client.user_timeline('joshsoftware').first.id
+      [client.status(id).full_text, client.status(id).created_at.strftime('%-d %b %H:%M %P')]
+    rescue
+      []
     end
-    #ids = client.user_timeline('joshsoftware').collect(&:id)
-    #ids[0..5].collect{|id| client.status(id).full_text}
-    id = client.user_timeline('joshsoftware').first.id
-    [client.status(id).full_text, client.status(id).created_at.strftime('%-d %b %H:%M %P')]
   end
 end
