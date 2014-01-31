2682583e89fffa15d4fdd884515ba02a0775e305
Rajan Barmukh <rajan@joshsoftware.com>
2014-01-31 14:55:09 +0530

    uncommented latest tweet part

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   private
 
   def load_tweet
-    @tweet = ["sample tweet", "24 Jan 21:23 pm"] #Tweet.latest_tweet('joshsoftware')
+    @tweet = Tweet.latest_tweet('joshsoftware')
   end
 end
