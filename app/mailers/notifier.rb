910bdb92c1f58e89e79eb660d17bd174ec12f010
Prasad Surase <prasad@joshsoftware.com>
2014-02-05 14:25:59 +0530

    Changed the mail title

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,6 +1,6 @@
 class Notifier < ActionMailer::Base
   def notify_contact(contact)
     @contact = contact
-    mail(to: 'info@joshsoftware.com', from: contact.email, subject: 'Contact from Josh site', message: contact.message)
+    mail(to: 'info@joshsoftware.com', from: contact.email, subject: 'Request for information', message: contact.message)
   end
 end
