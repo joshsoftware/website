ea8e3480b6bda9bf284432485d75494ab6f5ccde
anuja <anuja@joshsoftware.com>
2014-02-20 14:17:41 +0530

    Changed Geocoder (not working on production)

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,9 +7,6 @@ class ContactsController < ApplicationController
   end
 
   def create
-    geokit = Geokit::Geocoders::IpGeocoder.geocode(request.remote_ip)
-    ip_location = { ip: request.remote_ip, location: "#{geokit.city}, #{geokit.state}, #{geokit.country}" }
-    params[:contact].merge! ip_location 
     @title = 'Contact Us | Josh Software Private Limited'
     @contact = Contact.new params[:contact]
     #@contact.message = @contact.message[0..290]
