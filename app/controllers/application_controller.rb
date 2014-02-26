347f28fc0fe3123f9305504c7d78735936faf3c1
anuja <anuja@joshsoftware.com>
2014-02-26 17:45:46 +0530

    Initialize tweet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def load_tweet
-    @tweet = @@latest_tweet || Tweet.latest_tweet
+    @@latest_tweet = Tweet.latest_tweet if @@latest_tweet.empty?
+    @tweet = @@latest_tweet
   end
 end
