ab8e1d51cf40a5ae1f02fd77ed7d0a129b089124
Shailesh Patil <shailesh@joshsoftware.com>
2014-02-26 17:00:20 +0530

    Added brandscope in portfolio

--- config/initializers/projects.rb
+++ config/initializers/projects.rb
@@ -9,12 +9,27 @@ PROJECTS= [
     image: "altizon.png",
     description: "Altizon Datonis platform has the ability for connected devices to stream extremely large amounts of sensor data, securely, in real time. The platform also has the ability to perform real-time analytics on this data and influence decision making. As an example, a supervisor in a manufacturing plant can keep track of the operational efficiency of all the Altizon-connected machines in his plant, in real time and take corrective actions if he is falling behind schedule. Analytics on data gathered directly from his machines would help production planning, execution and maintenance.",
     technical: {
-        stack: "Rails 4.0.0, MongoDB, ElasticSearch",
-        deployment: "AWS-EC2",
         comments: "",
         tags: ["Rails 4.0.0", "MongoDB", "ElasticSearch", "AWS-EC2"]
     },
     case_study: "portfolio_pdf/altizon_datonis_case_study.pdf",
+    display: true
+  },
+  
+  
+  {
+    id: "brandscope",
+    title: "Brandscope",
+    country: "Australia",
+    flag: "au.gif",
+    year: "2013",
+    url: "http://brandscope.com",
+    image: "brandscope.png",
+    description: "Brandscope Pty. Ltd. is focused on providing a streamline solution and maximizes the selling process for suppliers, retailers and agents in Australian fashion and Action Sports industry. Through this platform a supplier gets immediate access to quality retailers using unique and dynamic online tools to launch their brand with speed and efficiency giving them the competitive edge as well as a retailer gets the desired brands where they can view and buy product for their stores. An agent can reach to a large number of retailers and suppliers across geographies in no time with negligible efforts.The delivered solution features high resolution photography and video, concise product descriptions, buy plans, PDF catalogue automation, live social networking and ratings systems to mimic the traditional “touch n feel” sales process and create a 24/7 on-line showing experience for the Retailer.",
+    technical: {
+        comments: "",
+        tags: ["Ruby 2.0", "Rails 3.2.13", "MongoDB", "nginx + passenger", "AWS-EC2", "FTP server"]
+    },
     display: true
   },
 
@@ -28,12 +43,26 @@ PROJECTS= [
     image: "educade.png",
     description: "Educade.org is a revolutionary online portal that answers the needs teachers have today and prepares students for success by providing free, ready to use lesson plans paired with engaging learning technologies. Tools include hundreds of apps, games, and hands-on activities with lessons aligned to Next Generation and Common Core standards, making it fast and easy to use. Educade is a platform for change with the mission to build a community around sharing engaging and interactive educational content.",
     technical: {
-        stack: "Rails 3.2.12, MongoDB, ElasticSearch",
-        deployment: "OpenStack",
         comments: "filter.js",
         tags: ["Rails 3.2.12", "MongoDB", "ElasticSearch", "OpenStack", "filter.js"]
     },
     case_study: "portfolio_pdf/educade_case_study.pdf",
+    display: true
+  },
+
+  {
+    id: "zenith",
+    title: "Zenith",
+    country: "Australia",
+    flag: "au.gif",
+    year: "2013",
+    url: "https://zenithhub.com",
+    image: "zenith.png",
+    description: "Zenith is printing solutions for kwik kopy clients. Clients can order, preview, update and print Business cards, Posters, Flyers, Files etc.",
+    technical: {
+        comments: "",
+        tags: ["Ruby 2.0", "Rails 3.2.13", "PostgreSQL", "ElasticSearch", "nginx + passenger", "AWS-EC2", "Infixserver", "Pdftron"]
+    },
     display: true
   },
 
@@ -48,8 +77,6 @@ PROJECTS= [
     description: "Phalcomm is an E-Commerce Platform which offers online buying of real estate, real time brokerage and generates project specific report. It is used by over 500+ distributor and agents of phalcomm. Software provides real time information of about various bookings in progress across the nation and thus avoid double booking of same property. It also tracks the workflow from booking till allotment of the unit to customer. 
     System is very rich in reports to help ground staff and management as well to take decisions based on the total sale by various distributors, enquiry to sale conversion ratio, demands in the market etc. Reports are also supported by various graphs for better visual understanding and comparison.",
     technical: {
-        stack: "Ruby 2.0, Rails 3.2.11, MongoDB, WebSockets, Sidekiq, Redis",
-        deployment: "Control-S",
         comments: "",
         tags: ["Ruby 2.0", "Rails 3.2.11", "MongoDB", "Sidekiq", "Redis", "WebSockets", "Control-S"]
     },
@@ -66,8 +93,6 @@ PROJECTS= [
     image: "shopgodt.png",
     description: "Shopgodt is the answer to what has been a lonely and research-intense experience to shopping. Get your friend's opinions and comments real time, find the best prices for that product, read reviews of the product, see celebrities who wore the product, read expert opinions from your favorite bloggers, all in ONE place and in REAL TIME!",
     technical: {
-        stack: "Rails 3.2.11, MongoDB, AWS S3",
-        deployment: "EngineYard",
         comments: "Omniauth, FB and Twitter login",
         tags: ["Rails 3.2.11", "MongoDB", "AWS S3", "EngineYard", "Omniauth", "FB and Twitter login"]
     },
@@ -84,8 +109,6 @@ PROJECTS= [
     image: "cleartrip.png",
     description: "This is a hotel directory listing for Cleartrip. Very heavy caching enables hotels search very fast and reliable. This page is used in A/B testing for Cleartrip. Localities are mapped and nearby hotels are cached via XML api access using Cleartrip APIs.",
     technical: {
-        stack: "Rails 3.0.7, MySQL, Memcached, nginx + passenger",
-        deployment: "AWS-EC2",
         comments: "",
         tags: ["Rails 3.0.7", "MySQL", "Memcached", "nginx + passenger", "AWS-EC2"]
     },
@@ -102,8 +125,6 @@ PROJECTS= [
     image: "tourconnect.png",
     description: "TourConnect makes managing rates and contracting quick and easy! TourConnect communicates what the Supplier product is and how much it's going to cost each Contractor",
     technical: {
-        stack: "Rails 3.1.3, MongoDB, Authorize.Net, ActiveAdmin",
-        deployment: "Heroku Cedar Stack",
         comments: "",
         tags: ["Rails 3.1.3", "MongoDB", "Authorize.Net", "ActiveAdmin", "Heroku Cedar Stack"]
 
@@ -121,8 +142,6 @@ PROJECTS= [
     image: "1factory.png",
     description: "1Factory is a portal that manages the quality control process for multi-tiered supply chain. This is a multi-tenant application with a lot of impetus on security.",
     technical: {
-        stack: "Rails 3.1, PostgreSQL",
-        deployment: "Heroku Cedar Stack",
         comments: "",
         tags: ["Rails 3.1", "PostgreSQL", "Heroku Cedar Stack"]
     },
@@ -139,8 +158,6 @@ PROJECTS= [
     image: "groupgyaan.png",
     description: "GroupGyaan is a next generation marketplace for classroom sessions. You can book tickets for sessions on things that may be of interest to you, from a variety of different areas such as photography, cooking, parenting or negotiation skills.",
     technical: {
-        stack: "Rails 3.0.6, PostgreSQL, GharPay, PayU, IndexTank",
-        deployment: "Heroku Cedar Stack",
         comments: "",
         tags: ["Rails 3.0.6", "PostgreSQL", "GharPay", "PayU", "IndexTank", "Heroku Cedar Stack"]
     },
@@ -157,8 +174,6 @@ PROJECTS= [
     image: "moveme.png",
     description: "This site helps in moving house in UK. You can get instant quotes from vendors for insurance, man and van etc. The Free House Moving Checklist is an ideal planner for managing your home move.<br> This is a while-lable solution for various portals.",
     technical: {
-        stack: "Rails 3.0.1, MySQL, Rabl",
-        deployment: "Private Hosting",
         comments: "white-lable solution",
         tags: ["Rails 3.0.1", "MySQL", "Rabl", "white-lable solution"]
     },
@@ -175,31 +190,12 @@ PROJECTS= [
     image: "ipalm.png",
     description: "'Integrated Print & Logistics Management' (iPALM) is a proprietary Technology platform. It moves traditional print, warehousing, and fulfilment capabilities online, providing customers with access to storefronts, backlist catalogues, fulfillment, tracking, reporting and print on demand.",
     technical: {
-        stack: "Rails 2.3.14, MySQL, Redis/Resque, liquid",
-        deployment: "Private Hosting",
         comments: "",
         tags: ["Rails 2.3.14", "MySQL", "Redis/Resque", "liquid", "Private Hosting"]
     },
     display: true
   },
 
-  {
-    id: "zenith",
-    title: "Zenith",
-    country: "Australia",
-    flag: "au.gif",
-    year: "2013",
-    url: "https://zenithhub.com",
-    image: "zenith.png",
-    description: "Zenith is printing solutions for kwik kopy clients. Clients can preview, update and print Business cards, Posters, Flyers, Files etc.",
-    technical: {
-        stack: "Ruby 2.0, Rails 3.2.13, PostgreSQL, ElasticSearch, Rackspace",
-        deployment: "Private Hosting",
-        comments: "",
-        tags: ["Ruby 2.0", "Rails 3.2.13", "PostgreSQL", "ElasticSearch", "Rackspace"]
-    },
-    display: true
-  },
 
   {
     id: "acemoney",
