df2110f0419930a455318f3f4d2120b2d81c7a04
anuja <anuja@joshsoftware.com>
2014-08-01 15:19:21 +0530

    Changed sender mail to marketing@joshsoftware.com

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,7 +3,7 @@ require "google_drive"
 class Notifier < ActionMailer::Base
   def notify_contact(contact)
     @contact = contact
-    mail(to: 'info@joshsoftware.com', from: contact.email, subject: 'Request for information', message: contact.message)
+    mail(to: 'info@joshsoftware.com', from: 'marketing@joshsoftware.com', reply_to: contact.email, subject: 'Request for information', message: contact.message)
     add_to_google_spreadsheet
   end
 
