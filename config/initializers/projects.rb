f1b653f9c2e4a5cfc3e4380baf3e311cee039cc7
anuja <anuja@joshsoftware.com>
2014-08-01 14:55:48 +0530

    Reduced size of images on portfolio

--- config/initializers/projects.rb
+++ config/initializers/projects.rb
@@ -6,7 +6,7 @@ PROJECTS= [
     flag: "india.gif",
     year: "2013",
     url: "http://altizon.com/altizon-datonis-platform/",
-    image: "altizon.png",
+    image: "altizon-scaled.png",
     description: "Altizon Datonis platform has the ability for connected devices to stream extremely large amounts of sensor data, securely, in real time. The platform also has the ability to perform real-time analytics on this data and influence decision making. As an example, a supervisor in a manufacturing plant can keep track of the operational efficiency of all the Altizon-connected machines in his plant, in real time and take corrective actions if he is falling behind schedule. Analytics on data gathered directly from his machines would help production planning, execution and maintenance.",
     technical: {
         comments: "",
@@ -24,7 +24,7 @@ PROJECTS= [
     flag: "au.gif",
     year: "2013",
     url: "http://brandscope.com.au",
-    image: "brandscope.png",
+    image: "brandscope-scaled.png",
     description: "Brandscope Pty. Ltd. is focused on providing a streamline solution and maximizes the selling process for suppliers, retailers and agents in Australian fashion and Action Sports industry. Through this platform a supplier gets immediate access to quality retailers using unique and dynamic online tools to launch their brand with speed and efficiency giving them the competitive edge as well as a retailer gets the desired brands where they can view and buy product for their stores. An agent can reach to a large number of retailers and suppliers across geographies in no time with negligible efforts.The delivered solution features high resolution photography and video, concise product descriptions, buy plans, PDF catalogue automation, live social networking and ratings systems to mimic the traditional “touch n feel” sales process and create a 24/7 on-line showing experience for the Retailer.",
     technical: {
         comments: "",
@@ -40,7 +40,7 @@ PROJECTS= [
     flag: "us.gif",
     year: "2013",
     url: "http://educade.org",
-    image: "educade.png",
+    image: "educade-scaled.png",
     description: "Educade.org is a revolutionary online portal that answers the needs teachers have today and prepares students for success by providing free, ready to use lesson plans paired with engaging learning technologies. Tools include hundreds of apps, games, and hands-on activities with lessons aligned to Next Generation and Common Core standards, making it fast and easy to use. Educade is a platform for change with the mission to build a community around sharing engaging and interactive educational content.",
     technical: {
         comments: "filter.js",
@@ -57,7 +57,7 @@ PROJECTS= [
     flag: "au.gif",
     year: "2013",
     url: "http://www.kwikkopy.com.au/zenith-online-print",
-    image: "zenith.png",
+    image: "zenith-scaled.png",
     description: "Zenith is printing solutions for kwik kopy clients. Clients can order, preview, update and print Business cards, Posters, Flyers, Files etc.",
     technical: {
         comments: "",
@@ -73,7 +73,7 @@ PROJECTS= [
     flag: "india.gif",
     year: "2013",
     url: "http://erp.phalcomm.com",
-    image: "phalcomm.png",
+    image: "phalcomm-scaled.png",
     description: "Phalcomm is an E-Commerce Platform which offers online buying of real estate, real time brokerage and generates project specific report. It is used by over 500+ distributor and agents of phalcomm. Software provides real time information of about various bookings in progress across the nation and thus avoid double booking of same property. It also tracks the workflow from booking till allotment of the unit to customer. 
     System is very rich in reports to help ground staff and management as well to take decisions based on the total sale by various distributors, enquiry to sale conversion ratio, demands in the market etc. Reports are also supported by various graphs for better visual understanding and comparison.",
     technical: {
@@ -90,7 +90,7 @@ PROJECTS= [
     flag: "us.gif",
     year: "2013",
     url: "http://shopgodt.com",
-    image: "shopgodt.png",
+    image: "shopgodt-scaled.png",
     description: "Shopgodt is the answer to what has been a lonely and research-intense experience to shopping. Get your friend's opinions and comments real time, find the best prices for that product, read reviews of the product, see celebrities who wore the product, read expert opinions from your favorite bloggers, all in ONE place and in REAL TIME!",
     technical: {
         comments: "Omniauth, FB and Twitter login",
@@ -106,7 +106,7 @@ PROJECTS= [
     flag: "in.gif",
     year: "2011",
     url: "http://www.cleartrip.com/hotels/india/pune/",
-    image: "cleartrip.png",
+    image: "cleartrip-scaled.png",
     description: "This is a hotel directory listing for Cleartrip. Very heavy caching enables hotels search very fast and reliable. This page is used in A/B testing for Cleartrip. Localities are mapped and nearby hotels are cached via XML api access using Cleartrip APIs.",
     technical: {
         comments: "",
@@ -139,7 +139,7 @@ PROJECTS= [
     flag: "us.gif",
     year: "2012",
     url: "http://1factory.com",
-    image: "1factory.png",
+    image: "1factory-scaled.png",
     description: "1Factory is a portal that manages the quality control process for multi-tiered supply chain. This is a multi-tenant application with a lot of impetus on security.",
     technical: {
         comments: "",
@@ -155,7 +155,7 @@ PROJECTS= [
     flag: "in.gif",
     year: "2011",
     url: "http://groupgyaan.com",
-    image: "groupgyaan.png",
+    image: "groupgyaan-scaled.png",
     description: "GroupGyaan is a next generation marketplace for classroom sessions. You can book tickets for sessions on things that may be of interest to you, from a variety of different areas such as photography, cooking, parenting or negotiation skills.",
     technical: {
         comments: "",
@@ -171,7 +171,7 @@ PROJECTS= [
     flag: "gb.gif",
     year: "2011",
     url: "http://planner.moveme.com",
-    image: "moveme.png",
+    image: "moveme-scaled.png",
     description: "This site helps in moving house in UK. You can get instant quotes from vendors for insurance, man and van etc. The Free House Moving Checklist is an ideal planner for managing your home move.<br> This is a while-lable solution for various portals.",
     technical: {
         comments: "white-lable solution",
@@ -187,7 +187,7 @@ PROJECTS= [
     flag: "au.gif",
     year: "2012",
     url: "http://www.opusgroup.com.au/ipalm",
-    image: "ipalm.png",
+    image: "ipalm-scaled.png",
     description: "'Integrated Print & Logistics Management' (iPALM) is a proprietary Technology platform. It moves traditional print, warehousing, and fulfilment capabilities online, providing customers with access to storefronts, backlist catalogues, fulfillment, tracking, reporting and print on demand.",
     technical: {
         comments: "",
