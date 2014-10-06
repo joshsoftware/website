13a19db9f0a6b3d6cebd80013ca74cee7fc4ffe3
Shweta <shweta@joshsoftware.com>
2014-10-06 15:40:43 +0530

    Recaptcha settings added.

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
   config.public_key  = '6LcJHvsSAAAAANmruZ9e8eYIq1TyJeZC4Zq6YhDG'
-  config.private_key = '6LcJHvsSAAAAAE9NfT2KeNqHm6FiQASrSI8jx6QO'
+  config.private_key = ENV['RECAPTCHA_API_KEY']
 end
