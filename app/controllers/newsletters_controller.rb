e7936df21b8db6ab113d417a5bf57091b20e4b70
Shweta <shweta@joshsoftware.com>
2016-07-09 14:22:54 +0530

    Updated url for newsletter

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -9,7 +9,7 @@ class NewslettersController < ApplicationController
     @user = NewsletterUser.new params[:newsletter_user]
     user_valid = @user.valid? ? true : false
     if verify_recaptcha(:model => @user, :message => "Oh! It's error with reCAPTCHA!", attribute: 'recaptcha') and user_valid
-      RestClient.post("http://joshsoftware.com/opt_in", {email: params[:newsletter_user][:email]}, {:accept => :json})
+      RestClient.post("http://intranet.joshsoftware.com:8080/opt_in", {email: params[:newsletter_user][:email]}, {:accept => :json})
       redirect_to newsletter_thankyou_path and return
     else
       render 'new'
