e695b895182946f2bf9448917270ae4513a82258
Sachin Kamble <sachin@clistesoft.com>
2018-07-09 20:51:01 +0530

    Added: Home page sections and new assets

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,11 @@
 (function(window, document, $) {
   'use strict';
 
+  $('[data-toggle="offcanvas"]').on('click', function () {
+    $('.offcanvas-collapse').toggleClass('open');
+    $('html').toggleClass('no-md-scroll');
+  });
+
   window.mod = {};
 
   // $(function() {
