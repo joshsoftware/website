1f7943f12f44a8624b6fd56b7e099399ebdd235e
Rupesh Gadekar <rupesh@clistesoft.com>
2018-07-25 17:13:39 +0530

    few js code changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -33,30 +33,30 @@
     });
   }
 
-  $(document).on('scroll', onScroll);
+  // $(document).on('scroll', onScroll);
 
   //smoothscroll
-  $('a[href^="#"]').on('click', function(e) {
-    e.preventDefault();
-    $(document).off('scroll');
+  // $('a[href^="#"]').on('click', function(e) {
+  //   e.preventDefault();
+  //   $(document).off('scroll');
 
-    $('a').each(function() {
-      $(this).removeClass('active');
-    })
-    $(this).addClass('active');
+  //   $('a').each(function() {
+  //     $(this).removeClass('active');
+  //   })
+  //   $(this).addClass('active');
 
-    var target = this.hash,
-      menu = target;
-    $target = $(target);
-    $('html, body').stop().animate({
-      'scrollTop': $target.offset().top + 2
-    }, 500, 'swing', function() {
-      window.location.hash = target;
-      $(document).on('scroll', onScroll);
-    });
-  });
+  //   var target = this.hash,
+  //     menu = target;
+  //   $target = $(target);
+  //   $('html, body').stop().animate({
+  //     'scrollTop': $target.offset().top + 2
+  //   }, 500, 'swing', function() {
+  //     window.location.hash = target;
+  //     $(document).on('scroll', onScroll);
+  //   });
+  // });
 
-  function onScroll(event) {}
+  // function onScroll(event) {}
 
   $(function() {
     scrollerInit();
