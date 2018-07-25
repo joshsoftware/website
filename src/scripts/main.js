0fab6b966692dd6d8c2e2558886eff0eb1bd0084
Shweta kale <shweta@joshsoftware.com>
2018-07-25 16:29:22 +0530

    Contact us form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,7 +7,7 @@
   });
 
   $('html, body').animate({
-    scrollTop: $('#openSource').offset().top
+    //scrollTop: $('#openSource').offset().top
   }, 'slow');
 
   window.mod = {};
@@ -24,4 +24,24 @@
   //   });
   // });
 
+  $('#contact-form').on('submit', function (event) {
+    var params = {};
+    params.name = $('#input-fname').val();
+    params.email = $('#inputEmail').val();
+    params.organization = $('#input-organization').val();
+    params.job_title = $('#inputcompany').val();
+    params.role = $('#exampleFormControlSelect1').val();
+    params.message = $('#exampleFormControlTextarea1').val();
+    params.phone = $('#inputnumber').val();
+    $.post('http://intranet.joshsoftware.com/api/v1/contact_us.json', {contact_us: params, 'g-recaptcha-response': grecaptcha.getResponse}, function(result, status) {
+      let form = $('.form-contact');
+      form[0].reset();
+      alert("Thanks for contacting us! We will get back to you soon!");
+    }).fail(function(result){
+      alert(result.responseJSON.errors);
+    });
+    event.preventDefault();
+  });
+
 })(window, document, jQuery);
+
