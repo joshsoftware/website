0131c963fd7c14273f05042aca7921ed3833180a
Shweta kale <shweta@joshsoftware.com>
2018-07-27 17:28:20 +0530

    updated gulp file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -35,7 +35,7 @@ gulp.task('copy', () => {
 
 gulp.task('default', ['clean'], cb => {
   return runSequence(
-    ['templates', 'styles', 'vendors', 'scripts'],
+    ['templates', 'styles', 'vendors', 'scripts', 'pdf'],
     ['fonts', 'images', 'minifyCss'],
     'copy',
     cb
