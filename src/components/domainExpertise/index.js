b3c75ab1a1f91655c00a5aa1be3bfc60bf695a01
Ashvini <ashvini@joshsoftware.com>
2019-08-16 08:31:19 +0530

    Fixed: nested routes for domain experties.

--- src/components/domainExpertise/index.js
+++ src/components/domainExpertise/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import DomainHeader from "./domainHeader.js";
 import DomainWork from "./domainWork.js";
 import ExpertiesJSON from "./experties.json"
+import * as routes from '../../routeConstants';
 
 const DomainExpertise = ({ match }) => {
+
+  return (
+    <>
+      <Route path={`${routes.DOMAIN_EXPERTISE}/:domain`} component={DomainExpertiseLyaout} />
+      <Redirect to={`${routes.DOMAIN_EXPERTISE}/health`} />
+    </>
+  )
+}
+
+const DomainExpertiseLyaout = ({ match }) => {
+  const selectedDomain = match.params.domain ? match.params.domain : "health";
+
   return (
     <section className="w-100 section-home">
-      <DomainHeader selectedDomain={match.params.domain} />
+      <DomainHeader selectedDomain={selectedDomain} />
       <DomainWork domainExpertiseData={ExpertiesJSON[match.params.domain]} />
-      {/* <DomainUpdates />  */}
     </section>
   )
 }
