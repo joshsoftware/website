cf6e57a112247f76b5f457620ea458d329d06525
amoludage <amoludage@joshsoftware.com>
2019-08-27 16:33:00 +0530

    Added team api for loading team details

--- src/components/team/index.js
+++ src/components/team/index.js
@@ -1,24 +1,34 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import Image from 'react-image-webp';
 import * as routeConstants from '../../routeConstants';
 import { Heading1 } from '../home/homeStyledComponents';
-
-const LINKED_IN_BASE_URL = "https://www.linkedin.com/in/";
+import { API_BASE_URL, API_PUBLIC_URL } from '../../globalConstants'
 const GITHUB_BASE_URL = "https://github.com/";
 const JOSH_CAREER_URL = "http://careers.joshsoftware.com/";
+const LINKED_IN_BASE_URL = "https://www.linkedin.com/in/";
 
 const OurTeam = (props) => {
-  const { leaders, members } = props;
+  const [ members, setMembers ] = useState([]),
+  [ leaders, setLeaders ] = useState([]),
+  [ loading, setLoading ] = useState(true);
 
-  return (
-    <Fragment>
-      {/* <section className="section-team section-banner">
-        <div className="heading-overlay">
-        </div><a href="#ourTeam" className="scroll-down"><i className="icon-angle-double-down"></i></a>
-      </section> */}
-      {/* <div style={{ height: 67 }} /> */}
+  useEffect(() => {
+    async function fetchData() {
+      fetch(`${API_BASE_URL}team`, {
+        method: 'get',
+        headers: { 'Content-Type': 'application/json' },
+      }).then(res => res.json())
+      .then(res => {
+        setMembers(res.members);
+        setLeaders(res.leaders)
+        setLoading(false);
+      })
+    }
+    fetchData();
+  }, []);
 
+<<<<<<< HEAD
       {/** JOSH LEADERS */}
       <section id="ourTeam" className="section-content section-leadership section-home">
         <Heading1>Josh Leadership</Heading1>
@@ -54,17 +64,64 @@ const OurTeam = (props) => {
                               <i className="icon-github"></i>
                             </a>
                           }
+=======
+  if(loading) {
+    return null;
+  } else {
+    return (
+      <Fragment>
+        {/* <section className="section-team section-banner">
+          <div className="heading-overlay">
+          </div><a href="#ourTeam" className="scroll-down"><i className="icon-angle-double-down"></i></a>
+        </section> */}
+        {/* <div style={{ height: 67 }} /> */}
+
+        {/** JOSH LEADERS */}
+        <section id="ourTeam" className="section-content section-leadership section-home">
+          <Heading1>Josh Leadership</Heading1>
+          <div className="container">
+            {/* <h1 className="section-heading">Josh Leadership</h1> */}
+            {/* <div className="section-header">
+              <h2 className="section-heading">Josh Leadership</h2>
+              <p>Re-engineering existing inefficient architectures with zero down-time and complete data integrity</p>
+            </div> */}
+            <div className="row">
+              {
+                leaders.map(leader => {
+                  const { public_profile, employee_detail } = leader;
+                  return (
+                    <div className="col-lg-4 col-sm-6" key={public_profile.name}>
+                      <div className="card card-team">
+                        <img src={`${API_PUBLIC_URL}${public_profile.image_medium_url}`} alt={public_profile.name} className="img-fluid" />
+                        <div className="card-body">
+                          <div className="person-info">
+                            <h5>{leader.name}</h5>
+                            <p className="designation">{employee_detail.designation}</p>
+                          </div>
+                          <div className="professional-platforms">
+                            {
+                              public_profile.linkedin_url && <a href={`${public_profile.linkedin_url}`} target="_blank" rel="noopener noreferrer">
+                                <i className="icon-linkedin"></i>
+                              </a>
+                            }
+                            {
+                              public_profile.github_handle && <a href={`${GITHUB_BASE_URL}${public_profile.github_handle}`} target="_blank" rel="noopener noreferrer">
+                                <i className="icon-github"></i>
+                              </a>
+                            }
+                          </div>
+>>>>>>> Added team api for loading team details
                         </div>
                       </div>
                     </div>
-                  </div>
-                )
-              })
-            }
+                  )
+                })
+              }
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
 
+<<<<<<< HEAD
       {/** JOSH NERDS */}
       <section className="section-content section-nerds">
         <div className="container">
@@ -89,20 +146,46 @@ const OurTeam = (props) => {
                               <i className="icon-github"></i>
                             </a>
                           }
+=======
+        {/** JOSH NERDS */}
+        <section className="section-content section-nerds">
+          <div className="container">
+            <h1 className="section-heading">Meet Our Nerds</h1>
+            <div className="row">
+              {
+                members.map(member => {
+                  const { public_profile } = member;
+                  return (
+                    <div className="col-lg-3 col-md-6 col-6" key={public_profile.name}>
+                      <div className="card card-team">
+                        <img src={`${API_PUBLIC_URL}${public_profile.image_medium_url}`} alt={public_profile.name} className="img-fluid" />
+                        <div className="card-body">
+                          <div className="person-info -nerds">
+                            <h5>{public_profile.name}</h5>
+                          </div>
+                          <div className="professional-platforms">
+                            {
+                              public_profile.github_handle && <a href={`${GITHUB_BASE_URL}${public_profile.github_handle}`} target="_blank" rel="noopener noreferrer">
+                                <i className="icon-github"></i>
+                              </a>
+                            }
+                          </div>
+>>>>>>> Added team api for loading team details
                         </div>
                       </div>
                     </div>
-                  </div>
-                )
-              })
-            }
-          </div>
+                  )
+                })
+              }
+            </div>
 
-          {/** Navigations */}
-          <div className="btn-wrap">
-            <Link to={routeConstants.ABOUT_US_URL} className="btn btn-gray">About Us</Link>
-            <a href={JOSH_CAREER_URL} className="btn btn-outline-gray">Career</a>
+            {/** Navigations */}
+            <div className="btn-wrap">
+              <Link to={routeConstants.ABOUT_US_URL} className="btn btn-gray">About Us</Link>
+              <a href={JOSH_CAREER_URL} className="btn btn-outline-gray">Career</a>
+            </div>
           </div>
+<<<<<<< HEAD
         </div>
       </section>
     </Fragment>
@@ -409,6 +492,12 @@ OurTeam.defaultProps = {
       githubId: ""
     }
   ]
+=======
+        </section>
+      </Fragment>
+    )
+  }
+>>>>>>> Added team api for loading team details
 }
 
 export default OurTeam;
