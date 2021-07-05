51f93eefd6b82933a5d40a5a0ab558f7ba103af3
Sangeeta Yadav <sangeeta.yadav@joshsoftware.com>
2021-07-05 17:57:37 +0530

    css fixes in testimonial page

--- src/shared-components/testimonialItem/testimonialItem.js
+++ src/shared-components/testimonialItem/testimonialItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, CardBody } from "reactstrap";
 import "./testimonialItem.css";
+import clientProfile from "../../assets/images/revampImages/unnamed.png";
 
 const TestimonialItem = ({
   clientProfile,
@@ -14,11 +15,11 @@ const TestimonialItem = ({
       <CardBody className="p-0">
         <div className="client-content d-flex">
           <div className="client-profile d-flex">
-            {/* <img
+            <img
               src={clientProfile}
               alt="profile icon"
               className="profile-img"
-            /> */}
+            />
             <div className="client-details">
               <h6 className="font-weight-bold">{clientName}</h6>
               <p className="client-designation-text font-weight-bold mb-2">
