0cedbfe7dea124209a2c6dc9bfa821ed2976ce09
Sangeeta Yadav <sangeeta.yadav@joshsoftware.com>
2021-07-16 13:03:51 +0530

    article component ui fixes

--- src/shared-components/article/article.js
+++ src/shared-components/article/article.js
@@ -6,18 +6,19 @@ import "./article.css";
 const Article = ({ articleDesc, articleTitle, articleImg, link }) => {
   return (
     <div className="article-item-wrap">
-      <div className="article-items">
-        <h1 className="text-capitalize font-weight-bold text-center">
-          {articleTitle}
-        </h1>
-        <div className="image-wrap">
-          <img
-            src={require(`../../assets/images/revampImages/${articleImg}`)}
-            alt="article"
-          />
+      <div className="article-items position-relative">
+        <div className="article-item-top-content">
+          <h1 className="text-capitalize font-weight-bold text-center">
+            {articleTitle}
+          </h1>
+          <div className="image-wrap">
+            <img
+              src={require(`../../assets/images/revampImages/${articleImg}`)}
+              alt="article"
+            />
+          </div>
+          <p className="mb-3">{articleDesc}</p>
         </div>
-        <p className="mb-3 overflow-hidden">{articleDesc}</p>
-
         <DefaultButton buttonText="vIEW MORE" link={link} />
       </div>
     </div>
