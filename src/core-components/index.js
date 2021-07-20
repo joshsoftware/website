97e107ea07cf8781bf7c19fafa4bbbe5b48d9944
Rupesh Gadekar <rupesh.gadekar@joshsoftware.com>
2021-07-20 20:14:52 +0530

    Added core-components

---
+++ src/core-components/index.js
@@ -0,0 +1,22 @@
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+} from "./Card";
+import Container from "./Container";
+import { Row, Col } from "./Grid";
+
+export {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Container,
+  Row,
+  Col,
+};
