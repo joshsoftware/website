a33808643aec14e2a07820cf8ea7cf2be1e72782
Sangeeta Yadav <sangeeta.yadav@joshsoftware.com>
2021-07-23 21:40:26 +0530

    technology page component ui

---
+++ src/shared-components/tile/tile.js
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Card, CardBody, CardTitle, CardText } from "reactstrap";
+import "./tile.css";
+
+const Tile = ({
+  tileImg,
+  tileTitle,
+  tileDescription,
+  tileTags,
+  horizontal,
+}) => {
+  return (
+    <Card
+      className={`tile-card border-0 ${
+        horizontal ? "horizontal-tile-card-body" : "vertical-tile-card-body"
+      }`}
+    >
+      <CardBody className="tile-card-body d-flex">
+        <img
+          src={require(`../../assets/images/revampImages/${tileImg}`)}
+          alt="tile img"
+        />
+        <div className="tile-details">
+          <ul className="tag-list list-unstyled">
+            {tileTags.map((item) => (
+              <li className="tag-list-item-wrap">
+                <span className="tag-list-item bg-white">{item.tileTag}</span>
+              </li>
+            ))}
+          </ul>
+          <CardTitle tag="h5" className="text-uppercase font-weight-bold">
+            {tileTitle}
+          </CardTitle>
+          <CardText className="overflow-hidden text-truncate-multi-line">
+            {tileDescription}
+          </CardText>
+        </div>
+      </CardBody>
+    </Card>
+  );
+};
+
+Tile.prototype = {
+  horizontal: PropTypes.bool,
+};
+export default Tile;
