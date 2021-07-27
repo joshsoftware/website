80e0a39e58414fb545ab4a8bfcf34cbb142c10bf
Rupesh Gadekar <rupesh.gadekar@joshsoftware.com>
2021-07-27 20:56:16 +0530

    Removed compile time errors/warnings and formatting changes

--- src/components/home/technolgies.js
+++ src/components/home/technolgies.js
@@ -1,75 +1,77 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-// import Image from 'react-image-webp';
+
+import { Container } from "../../core-components";
 import * as routes from "../../routeConstants.js";
 import { TECHNOLOGIES_USED } from "../../globalConstants";
 
-const Technologies = props => {
-    const { domainsData } = props;
-    return (
-            <Fragment>
-                <section id="clientsSlide" className="section-technologies">
-                    <div className="container"> 
-                    <ul className="technology-wrap d-flex flex-wrap align-items-center justify-content-center mb-0 p-0">
-                    {domainsData.map(domain => {
-                        return (
-                        <li className="d-flex">
-                        <Link
-                        to={`${routes.TECHNOLOGIES_URL}/${domain.urlParameter}`}
-                        className="text-decoration-none"
-                        >
-                            <img className="img-fluid" alt={domain.name} src={require(`../../assets/images/technologies/${domain.logo}`)}></img>
-                        </Link>
-                        </li>
-                        )
-                    })}
-                    </ul>
-                    </div>
-                </section>
-            </Fragment>
-        );
+const Technologies = (props) => {
+  const { domainsData } = props;
+  return (
+    <section id="clientsSlide" className="section-technologies">
+      <Container>
+        <ul className="technology-wrap d-flex flex-wrap align-items-center justify-content-center mb-0 p-0">
+          {domainsData.map((domain, i) => {
+            return (
+              <li key={i} className="d-flex">
+                <Link
+                  to={`${routes.TECHNOLOGIES_URL}/${domain.urlParameter}`}
+                  className="text-decoration-none"
+                >
+                  <img
+                    className="img-fluid"
+                    alt={domain.name}
+                    src={require(`../../assets/images/technologies/${domain.logo}`)}
+                  ></img>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </Container>
+    </section>
+  );
 };
 
 Technologies.defaultProps = {
-    domainsData: [
-        {
-          id: 4,
-          name: TECHNOLOGIES_USED.ror,
-          logo: "Ruby-new.png",
-          urlParameter: 'ruby-on-rails'
-        },
-        {
-          id: 3,
-          name: TECHNOLOGIES_USED.go,
-          logo: "gophar-new.png",
-          urlParameter: 'go'
-        },
-        {
-          id: 5,
-          name: TECHNOLOGIES_USED.java,
-          logo: "java-new.png",
-          urlParameter: 'java'
-        },
-        {
-          id: 1,
-          name: TECHNOLOGIES_USED.ios,
-          logo: "iOS-new.png",
-          urlParameter: 'ios'
-        },
-        {
-          id: 2,
-          name: TECHNOLOGIES_USED.android,
-          logo: "Android-new.png",
-          urlParameter: 'android'
-        },
-        {
-          id: 6,
-          name: TECHNOLOGIES_USED.angular,
-          logo: "Angular-new.png",
-          urlParameter: 'angular'
-        }
-      ],
-    
+  domainsData: [
+    {
+      id: 4,
+      name: TECHNOLOGIES_USED.ror,
+      logo: "Ruby-new.png",
+      urlParameter: "ruby-on-rails",
+    },
+    {
+      id: 3,
+      name: TECHNOLOGIES_USED.go,
+      logo: "gophar-new.png",
+      urlParameter: "go",
+    },
+    {
+      id: 5,
+      name: TECHNOLOGIES_USED.java,
+      logo: "java-new.png",
+      urlParameter: "java",
+    },
+    {
+      id: 1,
+      name: TECHNOLOGIES_USED.ios,
+      logo: "iOS-new.png",
+      urlParameter: "ios",
+    },
+    {
+      id: 2,
+      name: TECHNOLOGIES_USED.android,
+      logo: "Android-new.png",
+      urlParameter: "android",
+    },
+    {
+      id: 6,
+      name: TECHNOLOGIES_USED.angular,
+      logo: "Angular-new.png",
+      urlParameter: "angular",
+    },
+  ],
 };
 
 export default Technologies;
--- src/components/home/whatWeDoNew.js
+++ src/components/home/whatWeDoNew.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 import { Heading1 } from "./homeStyledComponents.js";
-import Image from 'react-image-webp';
+import Image from "react-image-webp";
 
 const WhatWeDoNew = (props) => {
   const { domains } = props;
@@ -11,19 +11,20 @@ const WhatWeDoNew = (props) => {
         <div className="what-we-do-section">
           <Heading1>What We Do</Heading1>
           <ul className="domain-card d-flex flex-wrap align-items-stretch justify-content-center p-0">
-            {domains.map(domain => {
+            {domains.map((domain, i) => {
               return (
-                <li className="domain-card-content text-center">
+                <li key={i} className="domain-card-content text-center">
                   <div className="domain-card-items d-flex">
                     <div className="abs">
-                    <Image
-                      src={require(`../../assets/images/home/${domain.logo}`)}
-                      webp={require(`../../assets/images/home/${domain.logo}.webp`)}
-                      alt={domain.title} />
-                    <h5>{domain.title}</h5>
-                    <p className="mb-0">{domain.subTitle}</p>
+                      <Image
+                        src={require(`../../assets/images/home/${domain.logo}`)}
+                        webp={require(`../../assets/images/home/${domain.logo}.webp`)}
+                        alt={domain.title}
+                      />
+                      <h5>{domain.title}</h5>
+                      <p className="mb-0">{domain.subTitle}</p>
                     </div>
-                    </div>
+                  </div>
                 </li>
               );
             })}
@@ -31,42 +32,48 @@ const WhatWeDoNew = (props) => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 WhatWeDoNew.defaultProps = {
   domains: [
     {
       logo: "Rescue.png",
       title: "Rescue Missions",
-      subTitle: "Re-engineer existing inefficient architectures with zero down-time and complete data"
+      subTitle:
+        "Re-engineer existing inefficient architectures with zero down-time and complete data",
     },
     {
       logo: "IPmultiplier.png",
       title: "IP Multiplier",
-      subTitle: "Build brand and business value to develop proprietary intellectual property assets."
+      subTitle:
+        "Build brand and business value to develop proprietary intellectual property assets.",
     },
     {
       logo: "Accelerate.png",
       title: "Accelerate Time to Market",
-      subTitle: "Ensure swift ‘Concept to Code’ delivery to ensure our clients beat their competition to market."
+      subTitle:
+        "Ensure swift ‘Concept to Code’ delivery to ensure our clients beat their competition to market.",
     },
     {
       logo: "domain.png",
       title: "Simplify Domain Complexities",
-      subTitle: "Focus on simplifying complexities in our client's industry through intelligent technological intervention"
+      subTitle:
+        "Focus on simplifying complexities in our client's industry through intelligent technological intervention",
     },
     {
       logo: "agile.png",
       title: "Agile Process",
-      subTitle: "Follow AGILE processes for rapid, high quality and timely delivery with no surprises."
+      subTitle:
+        "Follow AGILE processes for rapid, high quality and timely delivery with no surprises.",
     },
     {
       logo: "architecture.png",
       title: "Scalable Architecture",
-      subTitle: "Enable our solutions to sustain, grow and consistently perform at large scale of data and consumers"
-    }
-  ]
-}
+      subTitle:
+        "Enable our solutions to sustain, grow and consistently perform at large scale of data and consumers",
+    },
+  ],
+};
 
 export default WhatWeDoNew;
