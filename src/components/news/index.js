ea8bbf07e2b62534442193e1139294cb1250f112
Rupesh Gadekar <rupesh.gadekar@joshsoftware.com>
2021-07-29 12:30:53 +0530

    Added Article section in News page

--- src/components/news/index.js
+++ src/components/news/index.js
@@ -5,8 +5,9 @@ import { API_BASE_URL } from "../../globalConstants.js";
 
 import NewsBanner from "../../shared-components/newsBanner/newsBanner";
 import { Container } from "../../core-components";
+import Article from "../../shared-components/article/article";
 
-const News = () => {
+const News = ({ dataList }) => {
   const [newsData, setNewsData] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -44,11 +45,45 @@ const News = () => {
           )}
         </div>
       </Container>
+      <div className="article-list d-flex justify-content-center">
+        {dataList.map((dataListItems, i) => (
+          <Article
+            key={i}
+            articleDesc={dataListItems.articleDesc}
+            articleImg={dataListItems.articleImg}
+            articleTitle={dataListItems.articleTitle}
+            link={dataListItems.link}
+          />
+        ))}
+      </div>
     </section>
   );
 };
 
 News.defaultProps = {
   data: [],
+  dataList: [
+    {
+      articleTitle: "Talks",
+      articleDesc:
+        "Assorted talks by our associates about  the pathbreaking trends in technology, the problems it solve, and more.",
+      articleImg: "talks.jpeg",
+      link: "/talks",
+    },
+    {
+      articleTitle: "Publications",
+      articleDesc:
+        "Collection of publications which gives insights about our knowledge, experience and capabilities.",
+      articleImg: "publication.jpeg",
+      link: "/publications",
+    },
+    {
+      articleTitle: "Our White papers",
+      articleDesc:
+        "Information to guide our readers about complex issues like Technology and Business connections.",
+      articleImg: "our_white_paper.jpeg",
+      link: "/white-papers",
+    },
+  ],
 };
 export default News;
