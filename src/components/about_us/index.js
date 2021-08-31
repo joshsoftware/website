25c13a52224e3fc60806c87b8e2ce7deff161ea4
Sangeeta Yadav <sangeeta.yadav@joshsoftware.com>
2021-08-31 18:37:09 +0530

    about us page section ui

--- src/components/about_us/index.js
+++ src/components/about_us/index.js
@@ -1,12 +1,107 @@
 import React from "react";
 import Image from "react-image-webp";
+import Slider from "react-slick";
+import "./about-us.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
+const settings = {
+  dots: false,
+  arrows: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        slidesToShow: 1.2,
+        centerPadding: "40px",
+      },
+    },
+  ],
+};
+
+const activityData = [
+  {
+    title: "Josh Talks",
+    imgSrc: "josh-talk@2x.jpeg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut laboreLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut laboreLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore",
+  },
+  {
+    title: "Dev Hacks",
+    imgSrc: "dev-hacks@2x.jpeg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut laboreLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut laboreLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore",
+  },
+  {
+    title: "Open Source",
+    imgSrc: "open-source@2x.jpeg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut laboreLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut laboreLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore",
+  },
+
+  {
+    title: "Life Coach",
+    imgSrc: "life-coach@2x.jpeg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut laboreLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut laboreLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore",
+  },
+  {
+    title: "Debates",
+    imgSrc: "debates@2x.jpeg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut laboreLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut laboreLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore",
+  },
+];
 const AboutUs = () => {
   return (
     //
     <section className="josh-content">
       <div className="about-us-section">
-        <h2>About us</h2>
+        <div className="container">
+          <h2 className="font-weight-bold text-center">About us</h2>
+          <div className="tag-line-box text-center">
+            <span className="mb-0">
+              Our name stands for passion. And, passion for technology is who we
+              are.
+            </span>
+          </div>
+
+          <p>
+            The success of Josh Software is a direct reflection of the
+            magnificent team behind it! Their passion and enthusiasm have been
+            the greatest driving factors and are responsible for every milestone
+            of the brand. Here’s to every individual who has been part of this
+            wonderful journey!
+          </p>
+
+          <div className="programs-slider-wrapper">
+            <Slider {...settings}>
+              {activityData.map((slide, i) => (
+                <div
+                  key={i}
+                  className="slider-item d-flex align-items-center justify-content-center flex-column text-center"
+                >
+                  <h3 className="font-weight-bold mb-3">{slide.title}</h3>
+                  <img
+                    src={require(`../../assets/images/revampImages/${slide.imgSrc}`)}
+                    alt="program img"
+                  />
+                  <p className="mb-2 text-truncate-multi-line -four-line overflow-hidden">
+                    {slide.description}
+                  </p>
+                </div>
+              ))}
+            </Slider>
+          </div>
+        </div>
       </div>
     </section>
   );
