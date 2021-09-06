865628318f9274cd31cac6cb8cae2bcf7d13eab0
Sangeeta Yadav <sangeeta.yadav@joshsoftware.com>
2021-09-06 14:34:36 +0530

    about us card component changes

--- src/shared-components/aboutUsCard/aboutUsCard.js
+++ src/shared-components/aboutUsCard/aboutUsCard.js
@@ -5,16 +5,15 @@ import "./aboutUsCard.css";
 const AboutUsCard = ({ link }) => {
   return (
     <>
-      <div className="about-us-box p-3">
-        <h6 className="mb-3 font-weight-bold">About Us</h6>
-        <p className="mb-0">
+      <div className="about-us-box">
+        <h6 className="mb-2 font-weight-bold">About Us</h6>
+        <p className="mb-3">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
           eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet,
           consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
           laboreLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
           eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet,
-          consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-          labore.
+          consectetur adipisicing elit, sed do eiusmod tempor
         </p>
         <LinkButton buttonText="view more" />
       </div>
