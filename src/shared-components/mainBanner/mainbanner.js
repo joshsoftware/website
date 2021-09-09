8d32d26bfd03362626a3a29d4dc134b27efdf9af
Sangeeta Yadav <sangeeta.yadav@joshsoftware.com>
2021-09-09 12:55:50 +0530

    banner page added and after merge files added

--- src/shared-components/mainBanner/mainbanner.js
+++ src/shared-components/mainBanner/mainbanner.js
@@ -1,40 +1,29 @@
 import React from "react";
-import PropTypes from "prop-types";
 import "./mainBanner.css";
 
 const MainBanner = ({
-  pageCaption,
-  startTagLine,
-  tagText,
-  endTagLine,
-  bannerClassName,
+  className,
+  mainBannerTitle,
+  middleText,
+  endText,
+  startText,
 }) => {
   return (
-    <div className={`main-banner ${bannerClassName}`}>
-      {startTagLine && endTagLine && tagText && (
-        <p className="mb-0">
-          {startTagLine}
-          <span className="font-weight-bold text-uppercase mx-1">
-            {tagText}
-          </span>
-          {endTagLine}
-        </p>
-      )}
-      {pageCaption && (
-        <h1 className="pagecaption font-weight-bold text-white text-capitalize mb-0">
-          {pageCaption}
-        </h1>
-      )}
+    <div
+      className={`main-banner-wrap ${className} d-flex justify-content-center flex-column align-items-center`}
+    >
+      <h6 className="text-white">
+        {startText}
+        <span className="font-weight-bold text-uppercase mx-2">
+          {middleText}
+        </span>
+        {endText}
+      </h6>
+      <h1 className="font-weight-bold text-white text-capitalize mb-0">
+        {mainBannerTitle}
+      </h1>
     </div>
   );
-};
-
-MainBanner.propTypes = {
-  bannerClassName: PropTypes.string,
-};
-
-MainBanner.defaultProps = {
-  bannerClassName: "",
 };
 
 export default MainBanner;
