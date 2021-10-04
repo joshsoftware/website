f83d12ec1667d6342c7606c06efba54aa046da76
Swapnil Chincholkar <swapnil@joshsoftware.com>
2021-10-04 19:03:51 +0530

    Revert "UI/contact us (#173)"
    This reverts commit 3f45872c20b9a20cf57f51c66d013a35661b6129.

--- src/shared-components/index.js
+++ src/shared-components/index.js
@@ -1,6 +1,5 @@
 import DefaultButton from "./defaultButton/defaultButton";
 import MediaCard from "./mediaCard";
 import VideoCard from "./videoCard";
-import LinkButton from "./linkButton/linkButton";
 
-export { DefaultButton, MediaCard, VideoCard, LinkButton };
+export { DefaultButton, MediaCard, VideoCard };
