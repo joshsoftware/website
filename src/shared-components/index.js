ce010baffe89ebab2b437fa78975d54bae44950c
sangeetasg <75415451+sangeetasg@users.noreply.github.com>
2021-10-07 17:58:05 +0530

    UI/contact us new UI (#175)
    * images added for contact us page ui
    
    * contact us page ui
    
    * fixes in html file
    
    * contact us form ui fixes
    
    * color variable added
    
    * link button css updated
    
    * button component css fixes
    
    * removed unwanted css and one spacing fix
    
    * removed unwnted css
    
    * contact us pages css fixes
    
    * images size fixes
    
    * added recatcha field and some minor fixes
    
    * Removed unwanted code
    
    * select field css changes
    
    * select field color changes
    
    * fixes of select field in contact us page
    
    * contact us page select field issue
    
    * color change in select field
    
    * fixes in css in select field
    
    * removed unwanted css
    
    * link added for insta page in contact us form
    
    * added links to mobile and email
    
    * removed warnings

--- src/shared-components/index.js
+++ src/shared-components/index.js
@@ -1,5 +1,6 @@
 import DefaultButton from "./defaultButton/defaultButton";
 import MediaCard from "./mediaCard";
 import VideoCard from "./videoCard";
+import LinkButton from "./linkButton/linkButton";
 
-export { DefaultButton, MediaCard, VideoCard };
+export { DefaultButton, MediaCard, VideoCard, LinkButton };
