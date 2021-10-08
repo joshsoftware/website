292d95d0bf3e36230cdac804113fdb1cbfbb35e1
Sangeeta Yadav <sangeeta.yadav@joshsoftware.com>
2021-10-08 13:59:09 +0530

    onClick prop added for linkButton component

--- src/shared-components/linkButton/linkButton.js
+++ src/shared-components/linkButton/linkButton.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Button } from "reactstrap";
 import "./linkButton.css";
 
-const LinkButton = ({ className, buttonText, link, icon }) => {
+const LinkButton = ({ className, buttonText, onClick }) => {
   return (
     <>
       <Button
         color="link"
         className={`${className} link-btn text-uppercase text-white text-center font-weight-bold`}
+        onClick={onClick}
       >
         {buttonText}
       </Button>
