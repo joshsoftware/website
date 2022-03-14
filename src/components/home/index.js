77084b554daa023dbc9d1c87ab3ecf3de2efb739
sangeetasg <75415451+sangeetasg@users.noreply.github.com>
2022-03-14 19:01:17 +0530

    Revert "new year animation 2022 changes" (#185)
    * Revert "new year animation 2022 changes"
    
    This reverts commit 14cc697ba9ac1531d3c4c92555fdb84e5d18ebcc.
    
    * Address updated on contact us page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React from 'react'
 
-import HomePageBanner from "./HomePageBanner.js";
-import { Clients } from "./clients.js";
+import HomePageBanner from './HomePageBanner.js';
+import { Clients } from './clients.js';
 // import Testimonials from './testimonials.js';
 // import Company from "./company.js";
 // import DomainExpertise from './domainExpertise.js';
-import DomainExpertiseNew from "./domainExpertiseNew.js";
-import Technologies from "./technolgies.js";
+import DomainExpertiseNew from './domainExpertiseNew.js';
+import Technologies from './technolgies.js';
 // import WhatWeDo from './whatWeDo.js';
-import WhatWeDoNew from "./whatWeDoNew.js";
+import WhatWeDoNew from './whatWeDoNew.js';
 // import Innovations from './innovations.js';
-import InnovationsNew from "./innovationsNew.js";
+import InnovationsNew from './innovationsNew.js';
 // import SuccessStories from './successStories.js';
-import SuccessStoriesNew from "./successStoriesNew.js";
-import ContactUs from "./contactUs.js";
-import HomepageTestimonials from "./homepageTestimonials.js";
-import imgNewYear from "../../assets/images/new-year-animation-2022.gif";
+import SuccessStoriesNew from './successStoriesNew.js';
+import ContactUs from './contactUs.js';
+import HomepageTestimonials from './homepageTestimonials.js'
 
 const Home = () => {
+
   return (
     <>
       <main id="page-wrap">
@@ -37,23 +37,8 @@ const Home = () => {
         {/* <Company /> */}
         <ContactUs />
       </main>
-      <div className="snowflakes" aria-hidden="true">
-        <div className="snowflake">❅</div>
-        <div className="snowflake">❆</div>
-        <div className="snowflake">❅</div>
-        <div className="snowflake">❆</div>
-        <div className="snowflake">❅</div>
-        <div className="snowflake">❆</div>
-        <div className="snowflake">❅</div>
-        <div className="snowflake">❆</div>
-        <div className="snowflake">❅</div>
-        <div className="snowflake">❆</div>
-        <div className="snowflake">❅</div>
-        <div className="snowflake">❆</div>
-      </div>
-      <img src={imgNewYear} alt="new year" className="img-new-year" />
     </>
-  );
-};
+  )
+}
 
 export default Home;
