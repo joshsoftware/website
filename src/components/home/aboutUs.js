3115a703836a8685f747a74a546f4e787deb3973
Sangeeta Yadav <sangeeta.yadav@joshsoftware.com>
2022-03-15 16:53:03 +0000

    about us section on homepage

---
+++ src/components/home/aboutUs.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { DefaultButton } from "../../shared-components";
+const AboutUs = (props) => {
+  return (
+    <section className="about-section text-center">
+      <div className="container">
+        <h1 className="font-weight-bold">About Us</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+          labore sed do eiusmod tempor incididunt ut labore sed do eiusmod
+          tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit, sed do eiusmod tempor incididunt ut labore Lorem
+          ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore sed do eiusmod tempor incididunt ut labore
+          sed do eiusmod tempor incididunt ut labore
+        </p>
+        <DefaultButton
+          buttonText="View More"
+          className="d-flex justify-content-center"
+          link="/about-us"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default AboutUs;
